e85f63e9243908a3d55838bce19c4c873b4afbba
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-30 14:03:10 -0300

    chore: remove unused _reqwest function and change the way of generating the address

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use std::error::Error;
-use reqwest::Response;
+use reqwest::{Response, Url};
 use serde_json::json;
 use supervisor::NodeJsProc;
 
-const SSR_ADDR: &'static str = "localhost:1000";
+static SSR_ADDR: (&str, u16) = ("localhost", 1000);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let address = format_url(SSR_ADDR.0, &SSR_ADDR.1)?;
+
     let nodejs = match NodeJsProc::start_with_server_url(
         "nodejs-server/dist/ssr/ssr.js".into(),
-        SSR_ADDR.into(),
+        address.to_string(),
     ) {
         Err(err) => return Err(err.into()),
         Ok(process) => process,
     };
     
-    let resp = match make_ssr_req().await {
+    let resp = match make_ssr_req(&address).await {
         Err(err) => {
             println!("req err: {:#?}", err);
             return Ok(());
@@ -49,7 +50,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     Ok(())
 }
 
-async fn make_ssr_req() -> Result<Response, reqwest::Error> {
+fn format_url(host: &str, port: &u16) -> Result<Url, String> {
+    let host = if host.contains("://") { format!("{}:{}", host, port) } else { format!("http://{}:{}", host, port) };
+
+    match Url::parse(&host) {
+        Err(err) => return Err(format!("Some better error explaining that url is invalid: {}", err.to_string())),
+        Ok(url) => Ok(url),
+    }
+}
+
+
+async fn make_ssr_req(addr: &Url) -> Result<Response, reqwest::Error> {
     let mut props: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
     props.insert("auth".into(), json!({
         "user": "Inertia-Rust"
@@ -62,7 +73,8 @@ async fn make_ssr_req() -> Result<Response, reqwest::Error> {
         version: None,
     };
 
-    let render_endpoint = format!("http://{}/render", SSR_ADDR);
+    let mut render_endpoint = addr.clone();
+    render_endpoint.set_path("render");
     
     reqwest::Client::new()
         .get(render_endpoint)
@@ -71,38 +83,4 @@ async fn make_ssr_req() -> Result<Response, reqwest::Error> {
         .header("Accept", "application/json")
         .send()
         .await
-}
-
-async fn _reqwest() -> Result<(), Box<dyn Error>> {
-    let resp = make_ssr_req().await;
-
-    if resp.is_err() {
-        println!("req err: {:#?}", resp.unwrap_err());
-        return Ok(());
-    }
-
-    let resp = resp.unwrap();
-
-    let json_parsed = resp.json::<inertia::InertiaAppResponse>().await;
-    
-    if json_parsed.is_err() {
-        println!("json err: {:#?}", json_parsed.unwrap_err());
-        return Ok(());
-    }
-
-    let mut json_parsed = json_parsed.unwrap();
-
-    json_parsed.body = json_parsed.body.replace("&quot;", "\"").replace("<!-- -->", "");
-
-    assert_eq!(json_parsed, inertia::InertiaAppResponse {
-        body: "<div id=\"app\" data-page=\"{\"component\":\"Index\",\"props\":{\"auth\":{\"user\":\"Inertia-Rust\"}},\"url\":\"/\",\"version\":null}\"><h1>Hello Inertia-Rust</h1></div>".into(),
-        head: vec![
-            "<meta name=\"description\" content=\"Just a mocked head... Ha!\" inertia>".into(),
-            "<title inertia>Hello inertia!</title>".into(),
-        ]
-    });
-
-    println!("{:#?}", json_parsed);
-
-    Ok(())
 }
